4a4a9e6fcc41041a404909da3d0ce43f2ef7d700
Jhonatan Lopes <jhonatanlopes22@hotmail.com>
2024-06-20 19:44:21 -0300

    Adicionar arquivos de projeto.

---
+++ JOGO SORTEAR - 20-06-2024/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JOGO_SORTEAR___20_06_2024
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random dado = new Random();
+            int valor;
+            valor = dado.Next(1, 4);
+            if (valor == 1)
+            {
+                pictureBox1.Visible = true;
+                pictureBox2.Visible = false;
+                pictureBox3.Visible = false;
+            }
+            if (valor == 2)
+            {
+                pictureBox1.Visible = false;
+                pictureBox2.Visible = true;
+                pictureBox3.Visible = false;
+            }
+            if (valor == 3)
+            {
+                pictureBox1.Visible = false;
+                pictureBox2.Visible = false;
+                pictureBox3.Visible = true;
+            }
+        }
+    }
+}
